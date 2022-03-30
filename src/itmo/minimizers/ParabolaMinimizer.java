bc597e0e481bf1ea7eec4189ba40c4f6ff4a6a21
Aedilay <Edgar.saratovcev@gmail.com>
2022-03-30 21:59:54 +0300

    fix: ParabolaMinimizer now asks oracle only once per iteration

--- src/itmo/minimizers/ParabolaMinimizer.java
+++ src/itmo/minimizers/ParabolaMinimizer.java
@@ -8,11 +8,11 @@ public class ParabolaMinimizer implements Minimizer {
     public Interval minimize(Oracle oracle, double epsilon, double left, double right) {
         double fLeft = oracle.askValue(left);
         double fRight = oracle.askValue(right);
+        double mid = (left + right) / 2;
+        double fMid = oracle.askValue(mid);
         int iteration = 0;
 
         while (right - left > epsilon) {
-            double mid = (left + right) / 2;
-            double fMid = oracle.askValue(mid);
             var u = mid -
                     (Math.pow((mid - left), 2) * (fMid - fRight) - Math.pow((mid - right), 2) * (fMid - fLeft))
                     / (2 * ((mid - left) * (fMid - fRight) - (mid - right) * (fMid - fLeft)) );
@@ -25,7 +25,6 @@ public class ParabolaMinimizer implements Minimizer {
                 var temp = mid;
                 mid = u;
                 u = temp;
-
                 temp = fMid;
                 fMid = fu;
                 fu = temp;
@@ -33,11 +32,17 @@ public class ParabolaMinimizer implements Minimizer {
 
             if (fu < fMid) {
                 left = mid;
+                fLeft = fMid;
+                mid = u;
+                fMid = fu;
             } else if (fu > fMid) {
                 right = u;
+                fRight = fu;
             } else {
                 left = mid;
                 right = u;
+                mid = (left + right) / 2;
+                fMid = oracle.askValue(mid);
             }
             iteration++;
         }
